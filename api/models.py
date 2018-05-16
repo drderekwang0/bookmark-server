5763f4126b47d82777b3ad72df9506fcfe8d5392
Derek Wang <dr.derekwang0@gmail.com>
2018-05-16 01:26:45 -0400

    Add API read all bookmarks

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    title = models.TextField()
+    url = models.TextField()
+    time = models.DateTimeField()
+    read = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.url
--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Bookmark
+
+
+def get_bookmarks(request):
+    bookmarks = Bookmark.objects.all()
+
+    data = []
+    for bookmark in bookmarks:
+        data.append(
+            {
+                "title": bookmark.title,
+                "url": bookmark.url,
+                "time": bookmark.time,
+                "read": bookmark.read,
+                "deleted": bookmark.deleted,
+                "created_at": bookmark.created_at,
+                "updated_at": bookmark.updated_at,
+            }
+        )
+
+    return JsonResponse({"data": data})
