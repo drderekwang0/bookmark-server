5ff27bae48454eafed770a7f71bf97cc7f8bafca
Derek Wang <dr.derekwang0@gmail.com>
2018-05-16 02:00:52 -0400

    Add API batch add bookmarks

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.get_bookmarks, name="get_bookmarks")]
+urlpatterns = [
+    path("", views.get_bookmarks, name="get_bookmarks"),
+    path("batch_add", views.batch_add, name="batch_add"),
+]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
-from django.http import JsonResponse
+import json
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Bookmark
 
@@ -21,3 +24,17 @@ def get_bookmarks(request):
         )
 
     return JsonResponse({"data": data})
+
+
+@csrf_exempt
+def batch_add(request):
+    data = json.loads(request.body.decode("utf-8"))["data"]
+
+    bookmarks = []
+    for d in data:
+        bookmarks.append(
+            Bookmark(title=d["title"], url=d["url"], time=d["time"])
+        )
+    Bookmark.objects.bulk_create(bookmarks)
+
+    return HttpResponse(status=201)
