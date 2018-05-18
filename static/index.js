6f0479796d7abf689cb364d6714a4dce5abbff7b
Derek Wang <dr.derekwang0@gmail.com>
2018-05-18 01:10:15 -0400

    Add action "mark as read" and "delete"

--- static/index.js
+++ static/index.js
@@ -1,19 +1,47 @@
-async function main() {
-  const url = "/bookmarks/";
+const URL = "/bookmarks";
 
-  const response = await fetch(url);
+async function main() {
+  const response = await fetch(URL);
   let data = await response.json();
   data = data.data;
 
   const table = $("#data");
   for (let d of data) {
     table.append(`
-      <tr>
+      <tr data-id="${d.id}">
         <td><a href="${d.url}">${d.title}</a></td>
-        <td></td>
+        <td>
+          <a class="read-button"><i class="fas fa-check"></i></a>
+          <a class="delete-button"><i class="fas fa-trash"></i></a>
+        </td>
       </tr>
     `);
   }
+
+  $(".read-button").click(function(e) {
+    const id = getCurrentID(e);
+    requestBookmarkAction(id, "read");
+    getCurrentRow(e).remove();
+  });
+
+  $(".delete-button").click(function(e) {
+    const id = getCurrentID(e);
+    requestBookmarkAction(id, "delete");
+    getCurrentRow(e).remove();
+  });
+}
+
+function getCurrentRow(e) {
+  return $(e.currentTarget).parents("tr");
+}
+
+function getCurrentID(e) {
+  return getCurrentRow(e).data("id");
+}
+
+async function requestBookmarkAction(id, action) {
+  const url = `${URL}/${id}/actions/${action}`;
+  const response = await fetch(url);
 }
 
 main();
